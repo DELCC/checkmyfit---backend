1c608871969f5975a3d71aec5fc787531adadc38
Yoann9808 <yoann.pouliquen92@gmail.com>
2025-10-29 13:07:06 +0100

    route items upload & backgroundRemoval

--- routes/items.js
+++ routes/items.js
@@ -8,24 +8,54 @@ const uniqid = require("uniqid");
 const Item = require("../models/items");
 
 router.post("/upload", async (req, res) => {
-  if (!req.files || !req.files.photoFromFront) {
-    return res.status(400).json({ result: false, error: "No file uploaded" });
-  }
-
-  const photoPath = `./tmp/${uniqid()}.jpg`;
-
   try {
+    // check files
+    if (!req.files || !req.files.photoFromFront) {
+      return res
+        .status(400)
+        .json({ result: false, error: "Aucun fichier reçu" });
+    }
+    // save file in temp
+    const photoPath = `./tmp/${uniqid()}.jpg`;
     await req.files.photoFromFront.mv(photoPath);
 
-    const resultCloudinary = await cloudinary.uploader.upload(photoPath, {
-      background_removal: "cloudinary_ai",
-      transformation: [{ background: "white", crop: "fill" }],
+    // Upload to Cloudinary & delete file from temp
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    fs.unlinkSync(photoPath);
+
+    return res.json({
+      result: true,
+      original_url: resultCloudinary.secure_url,
+      public_id: resultCloudinary.public_id,
     });
-    res.json({ result: true, url: resultCloudinary.secure_url });
   } catch (error) {
-    res.status(500).json({ result: false, error: error.message });
-  } finally {
-    if (fs.existsSync(photoPath)) fs.unlinkSync(photoPath);
+    console.error("Erreur upload:", error);
+    return res.status(500).json({ result: false, error: error.message });
+  }
+});
+
+router.post("/removeBackground", async (req, res) => {
+  try {
+    const { cloudinaryPublicId } = req.body;
+    if (!cloudinaryPublicId) {
+      return res.status(400).json({ result: false, error: "Aucun ID reçu" });
+    }
+    // create new url with no background based on cloudinary url and ID
+    const transformedUrl = cloudinary.url(cloudinaryPublicId, {
+      transformation: [
+        { effect: "background_removal" },
+        { background: "white" },
+      ],
+      secure: true,
+    });
+
+    return res.status(200).json({
+      result: true,
+      message: "success with background removal",
+      transformedUrl,
+    });
+  } catch (error) {
+    return res.status(500).json({ result: false, error });
   }
 });
 
